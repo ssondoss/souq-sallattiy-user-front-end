fb0f1cc38946e3611661761d4e252e21389ea2c5
Sondos Marashdeh <sondosmarashdeh360@gmail.com>
2021-07-25 17:27:40 +0300

    link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiURL: 'http://192.168.100.10:8080/',
-  imageURL: 'http://192.168.100.10:8887/',
+  api: 'http://sallatiy.com/api/',
+  images: 'http://sallatiy.com/static/',
 };
